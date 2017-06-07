626ce66b2e23d8d945ebb3ed0d6aad7f233264fb
Oscar Leon <Oscar Leon>
2017-06-06 22:40:22 -0400

    add TODO /get/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express= require('express');
 var bodyParse = require('body-parser');
 
+const {ObjectID} =require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,6 +26,22 @@ app.get('/todos',(req, res)=>{
     res.send({todos});
   },(e)=>{
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req, res)=>{
+  var id =req.params.id;
+  if(!ObjectID.isValid(id)){
+      return res.status(400).send('id is not valid');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send('todo not found')
+    }
+    return res.status(200).send(todo);
+  }, (e) => {
+    return res.status(400).send(e);
   });
 });
 
